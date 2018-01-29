a13136049c5aa6c4fdb98fc5a60bc8be21311fcb
Petr Řepa <petr.repa@gmail.com>
2018-01-29 20:39:29 +0100

    Pridana funkce pro stazeni zvukove sady ze samba serveru.

---
+++ download_sound_set.sh
@@ -0,0 +1,6 @@
+#!/bin/bash  
+
+server_ip=$1
+home_folder=$2
+sound_set=$3
+smbclient //$server_ip/$home_folder -N -c "prompt;recurse;mget $sound_set"
--- system_functions.py
+++ system_functions.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
 import os
+import subprocess
 
 
 def play_sound(file_name):
     os.system('aplay ' + str(file_name))
 
 
-def download_sound_files():
+def download_sound_files_github():
     clone = "git clone https://github.com/kmzbrnoI/shZvuky"
     os.system(clone)
+
+
+def download_sound_files_samba(server_ip, home_folder, sound_set):
+    subprocess.call(["./download_sound_set.sh", server_ip, home_folder, sound_set]);
