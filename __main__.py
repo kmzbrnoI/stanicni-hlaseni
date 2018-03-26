950da859c0052b3209ece6b513db718882dac735
Petr Řepa <petr.repa@gmail.com>
2018-03-26 18:29:21 +0200

    Rozdeleni souboru tcp_listener na vice casti connect(), listen() - closes #5 register_device() - closes #9

--- __main__.py
+++ __main__.py
@@ -1,6 +1,12 @@
+import socket
+
 import network_services_client
-import udp_discover
 import system_functions
+import udp_discover
+
+
+class OutdatedVersionError(Exception):
+    pass
 
 
 def main():
@@ -8,13 +14,33 @@ def main():
         device_info = system_functions.DeviceInfo()
         server_ip, server_port = udp_discover.get_ip(device_info.server_name)
         client = network_services_client.NetworkServicesClient()
-        client.tcp_listener(5896)
+
+        for _ in range(5):
+            print("TCP pokus ", _ + 1)
+            client_socket, version = client.connect(server_ip, int(server_port))
+
+            if version >= 1:
+                registered_socket = client.register_device(client_socket)
+                if registered_socket:
+                    client.listen(registered_socket)
+
+            else:
+                raise OutdatedVersionError("Je potreba aktualizovat verzi..")
+
+    except network_services_client.TCPCommunicationEstablishedError:
+        print("Nepovedlo se navazani komunikace pres TCP")
+
+    except network_services_client.TCPTimeoutError:
+        print("TCP Timeout.")
 
     except udp_discover.ServerNotFoundError:
         print("Server nenalezen.")
 
     except udp_discover.UDPTimeoutError:
         print("UDP Timeout.")
+
+    except OutdatedVersionError:
+        print("Zastaralá verze na serveru.")
 
 
 if __name__ == "__main__":
--- network_services_client.py
+++ network_services_client.py
@@ -3,15 +3,21 @@
 import socket
 import time
 
+import message_parser
 import report_manager
 import system_functions
 
 
+class TCPCommunicationEstablishedError(socket.error):
+    pass
+
+
+class TCPTimeoutError(socket.timeout):
+    pass
+
+
 class NetworkServicesClient():
     def __init__(self):
-        self.connection_attempts = 0
-        self.communication_accomplished = False
-        self.broadcast_port = 0
         self.rm = report_manager.ReportManager()
         self.device_info = system_functions.DeviceInfo()
         self.server_ip = ""
@@ -20,101 +26,97 @@ class NetworkServicesClient():
                           'Os': 'trainType/osobni_vlak_cislo.ogg',
                           'Ku': 'stations/kurim.ogg', 'Bs': 'stations/veverska_bityska.ogg'}
 
-    def get_connection_attempts(self):
-        return self.connection_attempts
+    def listen(self, socket):
+        while True:
+            try:
 
-    def add_connection_attempts(self):
-        self.connection_attempts += 1
+                message = socket.recv(1024)
 
-    def tcp_listener(self, port):
+                while not message.decode('UTF-8').endswith('\n'):
+                    message += socket.recv(1024)
+
+                decoded_message = message.decode('UTF-8')
+
+                print(decoded_message)
+                if decoded_message == "ukoncit":
+                    break
+
+                if decoded_message == "zvuky":
+                    system_functions.download_sound_files_samba("10.0.0.32", "share", self.rm.sound_set)
+                    continue
+
+                if "-;PING" in decoded_message:
+                    continue
+
+                if 'SH' in decoded_message:
+                    data = message_parser.parse(decoded_message, ';')
+
+                    print(data)
+
+                    train_number = train_info_list[0]
+                    report_list = []
+
+                    report_list.append("salutation/vazeni_cestujici.ogg")
+                    report_list.append("salutation/prosim_pozor.ogg")
+                    report_list.append(self.word_list[train_info_list[1]])  # osobní vlak, rychlík
+
+                    report_list += self.rm.parse_train_number(train_number)
+
+                    report_list.append(self.word_list[basic_info_list[2]])  # odjede, prijede...
+
+                    self.rm.create_report(report_list)
+
+
+            except socket.error:
+                print("Nastala chyba pri prijmu dat...")
+
+            except socket.timeout:
+                print("TCP Timeout")
+
+    def connect(self, ip, port):
+
         try:
             clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # host = self.server_ip
-            host = socket.gethostname()
 
-            while (not self.communication_accomplished) and (self.get_connection_attempts() < 5):
-                self.add_connection_attempts()
+            clientsocket.settimeout(20)
+            clientsocket.connect((ip, port))
+            clientsocket.send("-;HELLO;1.0\n".encode('UTF-8'))
 
-                try:
-                    clientsocket.connect((host, port))
-                    self.communication_accomplished = True
-                except socket.error:
-                    print("Server neni zapnuty!")
-                    print("Pocet pokusu: ", self.get_connection_attempts())
-                    time.sleep(5)
-                    if self.get_connection_attempts() >= 5:
-                        print("Ukoncuji spojeni")
-                        break
-                    self.tcp_listener(port)
+            message = clientsocket.recv(1024)
 
-            if (self.communication_accomplished):
-                clientsocket.send("-;HELLO;1.0\n".encode('UTF-8'))  # nezapomínat na konec řádku!
-                # print("odeslano hello..")
-                print("Zarizeni je pripraveno k prijmu dat...")
-                message = clientsocket.recv(1024)
-                print(message.decode('UTF-8'))  # zkontrolovat verzi
-                register_message = self.device_info.area + ";SH;REGISTER\n";
-                clientsocket.send(register_message.encode('UTF-8'))  # musím na server odeslat registrační zprávu
-                while True:  # funkce listen()
-                    try:
+            if message:
+                hello_message = message_parser.parse(message.decode('UTF-8'), ";")
 
-                        message = clientsocket.recv(1024)
+                version = hello_message[-1]
 
-                        while not message.decode('UTF-8').endswith('\n'):
-                            message += clientsocket.recv(1024)
+                version = version.replace("\n", "")
+                version = version.replace("\r", "")
 
-                        decoded_message = message.decode('UTF-8')
+                version = float(version)
 
-                        if decoded_message == "ukoncit":
-                            break
-                        if decoded_message == "zvuky":
-                            system_functions.download_sound_files_samba("10.0.0.32", "share", self.rm.sound_set)
-                            continue
-                        if decoded_message != "-;PING\r\n":
-                            if 'SH' in decoded_message:
-                                data_to_read = decoded_message.partition('{')
-                                print(data_to_read)
-                                # přijatou zprávu rozdělím na časti podle znaku {
-                                basic_info = data_to_read[0]
-                                train_info = data_to_read[2].partition('}')[0]
+                print("Verze hello: ", version)
 
-                                basic_info_list = basic_info.split(";")
-                                # print("Message: ",decoded_message)
-                                train_info_list = train_info.split(";")
+                return (clientsocket, version)
 
-                                basic_info_list.pop()
-                                print("Basic info list:", basic_info_list)
-                                print("Train info list: ", train_info_list)
 
-                                """
-                                Priklad:
-                                Ku;SH;ODJEDE;
-                                504220;Os;1;Ku;Bs
-                                """
-
-                                train_number = train_info_list[0]
-                                report_list = []
-
-                                report_list.append("salutation/vazeni_cestujici.ogg")
-                                report_list.append("salutation/prosim_pozor.ogg")
-                                report_list.append(self.word_list[train_info_list[1]])  # osobní vlak, rychlík
-
-                                report_list += self.rm.parse_train_number(train_number)
-
-                                report_list.append(self.word_list[basic_info_list[2]])  # odjede, prijede...
-
-                                self.rm.create_report(report_list)
-
-                    except socket.error:
-                        print("Nastala chyba pri prijmu dat...")
-
-                print("Server ukoncil spojeni...")
-                clientsocket.close()
-                print("Ukoncuji tcp_listener...")
-
-            else:
-                print("Spojeni se nepovedlo navazat, zarizeni neni pripraveno k prijmu dat...")
+        except socket.error:
+            # raise TCPCommunicationEstablishedError("Server neni zapnuty!")
+            print("Nepodarilo se navazat komunikaci se serverem Error")
 
         except socket.timeout:
-            print("Nepovedlo se navazat spojeni!")
-            communication_accomplished = False
+            print("TCP Timeout")
+            # raise TCPTimeoutError("TCP Timeout")
+
+    def register_device(self, socket):
+        register_message = self.device_info.area + ";SH;REGISTER\n";
+        socket.send(register_message.encode('UTF-8'))  # musím na server odeslat registrační zprávu
+
+        message = socket.recv(1024)
+
+        while not message.decode('UTF-8').endswith('\n'):
+            message += clientsocket.recv(1024)
+
+        decoded_message = message.decode('UTF-8')
+
+        if decoded_message:  # tady potom přidat try, zda OŘ existuje
+            return socket
