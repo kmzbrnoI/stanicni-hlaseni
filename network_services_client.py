d82f15d65bf0a2feff4e4b1e4cc46cdafcbf49c0
Petr Řepa <petr.repa@gmail.com>
2018-04-17 13:17:20 +0200

    Prepsana trida network_services_client.py
    Opraveno prijmani dat do bufferu zprav
    Nastaven TCP timeout, pokud vyprsi probehne nove navazani spojeni. closes #14
    Pridano loggovani

--- network_services_client.py
+++ network_services_client.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+import logging
 import socket
-import threading, time
+import threading
+import time
 
 import message_parser
+import process_message
 import report_manager
 import system_functions
-import process_message
 
 
 class TCPCommunicationEstablishedError(socket.error):
@@ -17,138 +19,133 @@ class TCPTimeoutError(socket.timeout):
     pass
 
 
-class NetworkServicesClient():
+class OutdatedVersionError(Exception):
+    pass
+
+
+class NetworkServicesClient:
     def __init__(self):
         self.rm = report_manager.ReportManager()
         self.device_info = system_functions.DeviceInfo()
         self.server_ip = ""
         self.server_port = ""
+        self.connection_established = False
 
     def listen(self, socket):
         message_part = ""
+        buffer = []
+
         while True:
             try:
-                message = socket.recv(1024)
-                
-                if message_part :
-                    #předchozí zpráva došla po částech
+
+                message = socket.recv(2048)
+
+                socket.settimeout(60)
+
+                if message_part:
+                    # předchozí zpráva došla po částech
                     message = message_part.encode('UTF-8') + message
                     message_part = ""
-                          
-                while True :
-                    #pokud zprava neobsahuje \n přijímám dál...
-                    if '\n' in message.decode('UTF-8') :
-                        break
-                    else :
-                         message += socket.recv(1024)
 
+                while True:
+                    # pokud zprava neobsahuje \n přijímám dál...
+                    if '\n' in message.decode('UTF-8'):
+                        break
+                    else:
+                        message += socket.recv(2048)
+
+                logging.debug("Prijata zprava: {0}".format(message))
                 decoded_message = message.decode('UTF-8')
 
-                buffer = decoded_message.splitlines(True)
+                buffer += decoded_message.splitlines(True)
 
-                #print("Buffer :", buffer)
+                while buffer:
 
-                if buffer :
+                    if buffer[-1].endswith('\n'):  # vše ok
 
-                    if buffer[-1].endswith('\n'): #vše ok
+                        message_to_process = buffer.pop(0)
 
-                        message_to_process = buffer.pop(0) 
+                        if "-;HELLO" in message_to_process:
+                            self.process_hello_response(socket, message_to_process)
 
-                        if message_to_process == "ukoncit":
-                            break
+                        elif "REGISTER-RESPONSE;" in message_to_process:
+                            self.process_register_response(socket, message_to_process)
 
-                        if "-;PING" in message_to_process:
-                            continue
+                        elif ('SH' in message_to_process) and self.connection_established:
+                            logging.debug("Zpracovava se: {0}".format(message_to_process))
+                            threading.Thread(target=process_message.process_message(message_to_process)).start()
 
-                        if 'SH' in message_to_process:
-                            threading.Thread(target=process_message.process_message([message_to_process])).start()
-                            
-                    else :
-                        #poslední prvek připojím na začátek nové zprávy
+                    else:
+                        # vyhodím poslední prvek z bufferu a připojím na začátek nové zprávy
                         message_part = buffer.pop()
-                
-            except socket.error:
-                print("Nastala chyba pri prijmu dat...")
+                        break
 
-            except socket.timeout:
-                print("TCP Timeout")
+            except Exception:
+                logging.warning("TCP Timeout...")
+                break
+
+    def send_message(self, socket, message):
+        try:
+            socket.send(message.encode('UTF-8'))
+
+        except socket.error:
+            # raise TCPCommunicationEstablishedError("Server neni zapnuty!")
+            logging.error("Nepodarilo se odeslat zpravu na serveru...")
+
+        except socket.timeout:
+            logging.warning("TCP Timeout...")
+            # raise TCPTimeoutError("TCP Timeout")
+
+    def process_hello_response(self, socket, message):
+
+        hello_message = message_parser.parse(message, ";")
+        version = hello_message[-1]
+        version = version.replace("\n", "")
+        version = version.replace("\r", "")
+
+        version = float(version)
+
+        logging.debug("Verze hello: {0}".format(version))
+
+        if version >= 1:
+            register_message = self.device_info.area + ";SH;REGISTER\n"
+            self.send_message(socket, register_message)
+
+        else:
+            raise OutdatedVersionError("Je potreba aktualizovat verzi..")
+
+    def process_register_response(self, socket, message):
+        register_response = message_parser.parse(message, ";")
+
+        state = register_response[3]
+
+        state = state.replace("\n", "").replace("\r", "")
+
+        if state == 'OK':
+            self.connection_established = True
+            logging.info("Spojeni navazano.")
+        elif state == 'ERR':
+            error_note = register_response[4]
+            error_note = error_note.replace("\n", "").replace("\r", "")
+            if error_note == 'NONEXISTING_OR':
+                logging.error("Neexistujicí oblast řízení...")
+            elif error_note == 'ALREADY_REGISTERED':
+                logging.error("OŘ již byla registrovná.")
+            elif error_note == 'INTERNAL_ERROR':
+                logging.error("Vnitřní chyba serveru, více info na serveru")
 
     def connect(self, ip, port):
 
         try:
-            clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((ip, port))
 
-            clientsocket.settimeout(20)
-            clientsocket.connect((ip, port))
-            clientsocket.send("-;HELLO;1.0\n".encode('UTF-8'))
-
-            message = self.receive_special_message(clientsocket, "-;HELLO")
-            
-            if 'hello' in message.lower() : #muze se stat, ze zrovna prijde ping
-                
-                hello_message = message_parser.parse(message, ";")
-
-                version = hello_message[-1]
-
-                version = version.replace("\n", "")
-                version = version.replace("\r", "")
-
-                version = float(version)
-
-                print("Verze hello: ", version)
-
-                return (clientsocket, version)
-
+            return client_socket
 
         except socket.error:
             # raise TCPCommunicationEstablishedError("Server neni zapnuty!")
-            print("Nepodarilo se navazat komunikaci se serverem Error")
+            logging.warning("Nepodarilo se navazat komunikaci se serverem Error")
 
         except socket.timeout:
-            print("TCP Timeout")
+            logging.warning("TCP Timeout")
             # raise TCPTimeoutError("TCP Timeout")
-
-    def register_device(self, socket):
-        register_message = self.device_info.area + ";SH;REGISTER\n";
-        socket.send(register_message.encode('UTF-8'))  # musím na server odeslat registrační zprávu
-
-        message = self.receive_special_message(socket, "-;MOD-CAS")
-
-        if message:  # tady potom přidat try, zda OŘ existuje
-            return socket
-
-
-    def receive_special_message(self, socket, reply):
-        #metoda slouží pro příjem potvrzovací odpovědi na speciální zprávu napr. HELLO
-        message_part = ""
-        
-        message = socket.recv(512)
-        
-        if message_part :
-            #předchozí zpráva došla po částech
-            message = message_part.encode('UTF-8') + message
-            message_part = ""
-                  
-        while True :
-            #pokud zprava neobsahuje \n přijímám dál...
-            if '\n' in message.decode('UTF-8') :
-                break
-            else :
-                 message += socket.recv(512)
-
-        decoded_message = message.decode('UTF-8')
-
-        buffer = decoded_message.splitlines(True)
-
-        if buffer :
-
-            if buffer[-1].endswith('\n'): #vše ok
-
-                for item in buffer :
-                    #v bufferu mohou být klidně dvě zprávy
-                    if reply in item :
-                        return item
-                    
-            else :
-                #poslední prvek připojím na začátek nové zprávy
-                message_part = buffer.pop()
