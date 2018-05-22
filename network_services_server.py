ae4747c4f0415c25982b73fafabec11dca8e6c07
Petr Řepa <petr.repa@gmail.com>
2018-05-22 12:30:58 +0200

    Opraven emulator hJOP.

--- network_services_server.py
+++ network_services_server.py
@@ -2,11 +2,21 @@
 import socket
 
 
+def get_example_message(x):
+    return {
+        '1': 'Ku;SH;PRIJEDE;{501520;MOs;1;Br;Bs;;}\n',
+        '2': 'Ku;SH;ODJEDE;{504220;Os;1;Bs;Zd;;}\n',
+        '3': 'Ku;SH;SPEC;[NESAHAT]\n'
+    }.get(x, 'Ku;SH;ODJEDE;{504220;Os;1;Bs;Zd;;}\n')
+
+
 def tcp_listener():
+    area = "Ku"
+
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    host = socket.gethostname()
+    host = socket.gethostbyname(socket.gethostname())
 
     port = 5896
     serversocket.bind((host, port))
@@ -15,13 +25,35 @@ def tcp_listener():
     print("Nasloucham na portu: ", port);
 
     clientsocket, addr = serversocket.accept()
-    print("Navazano spojeni %s" % str(addr))
-    message = "hello"
-    clientsocket.send(message.encode('UTF-8'))
+    print("Navazano spojeni {0}".format(addr))
+    hello_msg = "-;HELLO;1.0\n"
+
+    while True:
+        message = clientsocket.recv(1024)
+        decoded_message = message.decode("UTF-8")
+        print("message: ", message)
+
+        if "HELLO" in str(message):
+            hello_msg = "-;HELLO;1.0\n"
+            clientsocket.send(hello_msg.encode('UTF-8'))
+
+        elif "SH;REGISTER" in message.decode('UTF-8'):
+            register_msg = area + ";SH;REGISTER-RESPONSE;OK;\n"
+            clientsocket.send(register_msg.encode('UTF-8'))
+            break
+
+        else:
+            ping_msg = "PING;\n"
+            clientsocket.send(ping_msg.encode('UTF-8'))
+
     while True:
         try:
-            input("Zadejte data pro odeslani: ")
-            message = "Zd;SH;ODJEDE;{504351;Os;7b;Zd;Klb}"
+            message = ""
+            print("Zadejte typ zpravy, ktery se odesle.\n1 : PRIJEDE\n2 : ODJEDE\n3 : NESAHAT ")
+            type = input()
+            message = get_example_message(type)
+            print("zprava: ", message)
+            
             clientsocket.send(message.encode('UTF-8'))
             if message == "ukoncit":
                 print("Spojeni bylo ukonceno prikazem...")
@@ -30,19 +62,22 @@ def tcp_listener():
             clientsocket.send(("ukoncit").encode('UTF-8'))
             break
 
+        except (KeyboardInterrupt, EOFError):
+            clientsocket.send(("ukoncit").encode('UTF-8'))
+            break
+
     clientsocket.close()
 
 
 def udp_listener():
-    sock = socket.socket(socket.AF_INET,  # Internet
-                         socket.SOCK_DGRAM)  # UDP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', 5889))
 
     while True:
-        data, addr = sock.recvfrom(4096)  # buffer size is 1024 bytes
-        print(data)
-        name = "hJOP;1.0;panel;"
-        if str(name) in str(data):
+        data, addr = sock.recvfrom(4096)
+        print("UDP_listener {0}".format(data))
+        connected = "hJOP;1.0;panel;"
+        if connected in str(data):
             break
 
 
@@ -58,5 +93,6 @@ def udp_broadcast(data):
 
 
 udp_listener()
-udp_broadcast("hJOP;1.0;server;server H0;10.30.137.10;5823;on;MujServer")
+current_ip = socket.gethostbyname(socket.gethostname())
+udp_broadcast("hJOP;1.0;server;server H0;" + current_ip + ";5896;on;hJOPEmulator")
 tcp_listener()
