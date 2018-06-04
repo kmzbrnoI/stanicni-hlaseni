a75127e83dd936e23fd4e744f79d1a4c7d1355fd
Petr Řepa <petr.repa@gmail.com>
2018-06-04 21:11:38 +0200

    Nepripojovat se k serveru, pokud je v režimu off.

--- udp_discover.py
+++ udp_discover.py
@@ -7,6 +7,7 @@ import system_functions
 class ServerNotFoundError(Exception):
     pass
 
+
 class UDPTimeoutError(socket.timeout):
     pass
 
@@ -25,7 +26,7 @@ def udp_broadcast(port, data):
 def get_ip(name):
     port = 5880
     # Metoda slouží pro odeslání informací o zařízení (RPI) na server a získání odpovědi skrze broadcast od serveru.
-    
+
     for _ in range(3):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -51,13 +52,16 @@ def get_ip(name):
                     device = device_info
 
                 if device:
-                    print("Nalezen server: {0}".format(device))
+                    logging.debug("Nalezen server: {0}".format(device))
 
                     server = str(device).split(";")
                     server_ip = server[4]
                     server_port = server[5]
-                    
-                    return (server_ip, server_port)
+                    is_on = server[6]
+                    if "on" in is_on:
+                        return (server_ip, server_port)
+                    else:
+                        break
 
         except socket.timeout:
             print("Nedosla odpoved na UDP (timeout)")
